18d638ef1ec5224e8d2d2bc42b94f1a319868f7b
vivekamin <aminvivek123@gmail.com>
2018-02-21 13:01:49 -0700

    Test for contract tried

--- inbox/test/inbox.test.js
+++ inbox/test/inbox.test.js
@@ -1,7 +1,92 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-const web3 = new Web3(ganache.provider());
+
+const provider = ganache.provider();
+const web3 = new Web3(provider);
+const { interface, bytecode } = require('../compile')
+
+
+let accounts; 
+let inbox;
+
+beforeEach( async () => {
+    /* --using promise--
+    web3.eth.getAccounts()
+        .then( fetchAccounts => {
+            console.log(fetchAccounts);      
+        });
+    */
+    accounts = await web3.eth.getAccounts();
+
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['Hi contract']})
+        .send({ from: accounts[0], gas : '1000000'});
+
+    inbox.setProvider(provider);
+});
+
+describe('Inbox contract', () => {
+
+    it('deploys a contract', () =>{
+        assert.ok(inbox.options.address);
+    });
+
+    it('has a default message', async () => {
+        const message = await inbox.methods.message().call();
+        assert.equal(message, 'Hi contract');
+        
+    });
+
+    it('can change the message', async () => {
+        await inbox.methods.setMessage('trasaction').send({ from: accounts[0] });
+        const message = await inbox.methods.message().call();
+        assert.equal(message, 'trasaction');
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+Mocha Test Sample
 
 class Car{
 
@@ -30,3 +115,4 @@ describe('class Car', ()=>{
     });
 
 });
+*/
