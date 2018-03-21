c8ad2cb72f30f80ec8c07019c3d44870eab70f91
vivekamin <aminvivek123@gmail.com>
2018-03-21 11:25:06 -0700

    problem resolved

---
+++ react-app/src/index.js
@@ -0,0 +1,62 @@
+import _ from 'lodash';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom'
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_details';
+
+const API_KEY = process.env.API_KEY;
+
+
+
+class App extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            videos:[],
+            selectedVideo: null,
+        };
+        this.videoSearch('eminem');
+    }
+
+    videoSearch(term){    
+        YTSearch({key: API_KEY, term: term}, (videos) => {
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
+            /*
+
+            this.setState({
+                videos: videos
+            });
+
+            */
+        })
+    }
+
+    render(){
+
+    const videoSearch = _.debounce((term)=>{
+        this.videoSearch(term)
+    },300);
+
+    return (
+    <div>
+        <SearchBar onSearchTermChange= {videoSearch} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+         onVideoSelect= {selectedVideo => this.setState({selectedVideo})}
+         videos={this.state.videos} />
+
+    </div>
+    )
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
+
+
---
+++ react-app/webpack.config.js
@@ -0,0 +1,31 @@
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  plugins: [
+    new Dotenv()
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015', 'stage-1']
+      }
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './'
+  }
+};
